1d95de641913244f11ea957c4369a957c898dc0d
Jason Garland <jason_garland@gap.com>
2016-10-25 18:05:28 -0400

    upgrade to google-api-client 0.9

--- reddit_to_youtube.rb
+++ reddit_to_youtube.rb
@@ -1,62 +1,60 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-gem 'google-api-client', '>0.7'
-require 'google/api_client'
-require 'google/api_client/client_secrets'
-require 'google/api_client/auth/file_storage'
-require 'google/api_client/auth/installed_app'
+gem 'google-api-client', '~> 0.9'
+require 'google/apis/youtube_v3'
+gem 'googleauth', '~> 0.5'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
 
 require 'json'
 require 'open-uri'
+require 'rack/utils'
 require 'pp'
+
 
 YOUTUBE_SCOPE = 'https://www.googleapis.com/auth/youtube'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
-def get_authenticated_service
-  client = Google::APIClient.new(
-    :application_name => $PROGRAM_NAME,
-    :application_version => '1.0.0'
-  )
-  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-  file_storage = Google::APIClient::FileStorage.new("#{$PROGRAM_NAME}-oauth2.json")
-  if file_storage.authorization.nil?
-    client_secrets = Google::APIClient::ClientSecrets.load
-    flow = Google::APIClient::InstalledAppFlow.new(
-      :client_id => client_secrets.client_id,
-      :client_secret => client_secrets.client_secret,
-      :scope => [YOUTUBE_SCOPE]
-    )
-    client.authorization = flow.authorize(file_storage)
-  else
-    client.authorization = file_storage.authorization
+def get_authenticated_service
+
+  youtube = Google::Apis::YoutubeV3::YouTubeService.new
+
+  client_id = Google::Auth::ClientId.from_file('client_secrets.json')
+  token_store = Google::Auth::Stores::FileTokenStore.new(
+    :file => "#{$PROGRAM_NAME}-oauth2.yaml")
+  authorizer = Google::Auth::UserAuthorizer.new(client_id, YOUTUBE_SCOPE, token_store)
+
+  user_id = client_id.id
+
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(base_url: OOB_URI )
+    puts "Open #{url} in your browser and enter the resulting code:"
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
   end
 
-  return client, youtube
+  youtube.authorization = credentials
+
+  return youtube
+
 end
 
 def get_video_data(video_ids)
-  client, youtube = get_authenticated_service
+  youtube = get_authenticated_service
 
   begin
       next_page_token = ''
       until next_page_token.nil?
-        videos_list_response = client.execute!(
-          :api_method => youtube.videos.list,
-          :parameters => {
-            :part => 'snippet',
-            :id => video_ids.join(","),
-            :pageToken => next_page_token
-          }
-        )
+        videos_list_response = youtube.list_videos('snippet', :id => video_ids.join(","), :page_token => next_page_token)
 
-        return videos_list_response.data.items
-
-        videos_list_response.data.items.each do |video_item|
-          puts video_item.inspect
+        videos_list_response.items.each do |video_item|
+          pp video_item
           #title = playlist_item['snippet']['title']
           #video_id = playlist_item['snippet']['resourceId']['videoId']
         end
@@ -64,14 +62,14 @@ def get_video_data(video_ids)
         next_page_token = videos_list_response.next_page_token
 
     end
-  rescue Google::APIClient::TransmissionError => e
-    puts e.result.body
+  rescue Google::Apis::ClientError => e
+    pp e
   end
 
 end
 
 def get_playlists()
-  client, youtube = get_authenticated_service
+  youtube = get_authenticated_service
 
   playlists = []
 
@@ -79,30 +77,21 @@ def get_playlists()
       # Retrieve the list of videos uploaded to the authenticated user's channel.
       next_page_token = ''
       until next_page_token.nil?
-        playlists_response = client.execute!(
-          :api_method => youtube.playlists.list,
-          :parameters => {
-            :mine => true,
-            :part => 'snippet',
-            :maxResults => 50,
-            :pageToken => next_page_token
-          }
-        )
-
+        playlists_response = youtube.list_playlists('snippet', :page_token => next_page_token, :mine => true, :max_results => 50)
 
         ## Print information about each video.
-        playlists_response.data.items.each do |playlist|
+        playlists_response.items.each do |playlist|
           data = {}
-          data['title'] = playlist['snippet']['title']
-          data['id'] = playlist['id']
+          data['title'] = playlist.snippet.title
+          data['id'] = playlist.id
           playlists.push(data)
         end
 
         next_page_token = playlists_response.next_page_token
 
     end
-  rescue Google::APIClient::TransmissionError => e
-    puts e.result.body
+  rescue Google::Apis::ClientError => e
+    pp e
   end
 
   return playlists
@@ -110,7 +99,7 @@ end
 
 
 def get_playlist_items(uploads_list_id)
-  client, youtube = get_authenticated_service
+  youtube = get_authenticated_service
 
   video_ids = []
 
@@ -118,59 +107,47 @@ def get_playlist_items(uploads_list_id)
       # Retrieve the list of videos uploaded to the authenticated user's channel.
       next_page_token = ''
       until next_page_token.nil?
-        playlistitems_response = client.execute!(
-          :api_method => youtube.playlist_items.list,
-          :parameters => {
-            :playlistId => uploads_list_id,
-            :part => 'snippet',
-            :maxResults => 50,
-            :pageToken => next_page_token
-          }
-        )
+        playlistitems_response = youtube.list_playlist_items('snippet', :playlist_id => uploads_list_id, :max_results => 50, :page_token => next_page_token)
 
         # Print information about each video.
-        playlistitems_response.data.items.each do |playlist_item|
-          title = playlist_item['snippet']['title']
-          video_id = playlist_item['snippet']['resourceId']['videoId']
+        playlistitems_response.items.each do |playlist_item|
+          title = playlist_item.snippet.title
+          video_id = playlist_item.snippet.resource_id.video_id
           video_ids.push(video_id)
         end
 
         next_page_token = playlistitems_response.next_page_token
 
     end
-  rescue Google::APIClient::TransmissionError => e
-    puts e.result.body
+  rescue Google::Apis::ClientError => e
+    pp e
   end
 
   return video_ids
 end
 
 def playlist_insert(play_list_id, video_id, note)
-  client, youtube = get_authenticated_service
+  youtube = get_authenticated_service
 
     body = {
       :snippet => {
-        :playlistId => play_list_id,
-        :resourceId => {
+        :playlist_id => play_list_id,
+        :resource_id => {
           :kind => 'youtube#video',
-          :videoId => video_id
+          :video_id => video_id
         }
       },
-      :contentDetails => {
-      	:note => note
+      :content_details => {
+        :note => note
       }
     }
 
+    item = Google::Apis::YoutubeV3::PlaylistItem.new(body)
+
   begin
-        playlistitems_response = client.execute!(
-          :api_method => youtube.playlist_items.insert,
-          :body_object => body,
-          :parameters => {
-            :part => 'snippet,contentDetails'
-          }
-        )
-  rescue Google::APIClient::TransmissionError => e
-    puts e.result.body
+    playlistitems_response = youtube.insert_playlist_item('snippet,contentDetails', item)
+  rescue Google::Apis::ClientError => e
+    pp e
   end
 
 end
@@ -184,10 +161,16 @@ def get_reddit_feed(sub_reddits)
       json << feed.read
     end
     parsed = JSON.parse(json)
-    return parsed    
+    return parsed
   rescue OpenURI::HTTPError => e
-    puts e.inspect
-    exit 1
+    if e.io.status.first.to_i == 429
+      puts "Reddit says \"#{e.io.status.last}\". Sleeping for 5 seconds and trying again."
+      sleep 5
+      return get_reddit_feed(sub_reddits)
+    else
+      pp e.io
+      exit 1
+    end
   end
 end
 
@@ -197,18 +180,27 @@ def get_reddit_links(sub_reddits)
   video_ids = []
 
   reddit_feed = get_reddit_feed(sub_reddits)
-  
-  reddit_feed['data']['children'].each do |item|  	
+
+  reddit_feed['data']['children'].each do |item|
     if item['data']['domain'] =~ /(youtube\.com|youtu\.be)/
       #links.push(item['data']['url'])
       uri = URI.parse(item['data']['url'])
-      if uri.host == "youtu.be"
+      if uri.host == 'youtu.be'
         vid_id = uri.path[1..-1]
         item['data']['video_id'] = vid_id
         video_ids.push(item['data'])
       else
         if uri.query
-          vid_id = uri.query.sub(/.*v=([a-zA-Z0-9\-\_]+).*/, '\1')
+          rack_parse = Rack::Utils.parse_query(uri.query)
+          if rack_parse.has_key?('v')
+            vid_id = rack_parse['v']
+          else
+            if uri.path =~ /attribution_link/ && rack_parse.has_key?('u')
+              new_uri = URI.parse(uri.scheme + '//' + uri.host + rack_parse['u'])
+              sub_rack_parse = Rack::Utils.parse_query(new_uri.query)
+              vid_id = sub_rack_parse['v'] if sub_rack_parse.has_key?('v')
+            end
+          end
           item['data']['video_id'] = vid_id
           video_ids.push(item['data'])
         end
@@ -219,31 +211,27 @@ def get_reddit_links(sub_reddits)
 end
 
 def new_playlist(title)
-  client, youtube = get_authenticated_service
+  youtube = get_authenticated_service
 
     body = {
       :snippet => {
         :title => title,
-        :resourceId => {
+        :resource_id => {
           :kind => 'youtube#playlist'
         }
       },
       :status => {
-        :privacyStatus => 'public'
+        :privacy_status => 'public'
       }
     }
 
+    new_playlist = Google::Apis::YoutubeV3::Playlist.new(body)
+
   begin
-        playlistsinsert_response = client.execute!(
-          :api_method => youtube.playlists.insert,
-          :body_object => body,
-          :parameters => {
-            :part => 'snippet,status'
-          }
-        )
-        return playlistsinsert_response.data['id']
-  rescue Google::APIClient::TransmissionError => e
-    puts e.result.body
+        playlistsinsert_response = youtube.insert_playlist('snippet,status', new_playlist)
+        return playlistsinsert_response.id
+  rescue Google::Apis::ClientError => e
+    pp e
   end
 
 end
